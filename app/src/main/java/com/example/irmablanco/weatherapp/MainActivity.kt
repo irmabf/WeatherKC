d1dc1cdded8f635c9f71d520975c3f44a771e9a1
Irma <irblancofdz@gmail.com>
2018-05-29 19:48:45 +0200

    Add resources

--- app/src/main/java/com/example/irmablanco/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/irmablanco/weatherapp/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.irmablanco.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //C
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+
+        outState?.putInt("Numero", 3)
     }
 }
