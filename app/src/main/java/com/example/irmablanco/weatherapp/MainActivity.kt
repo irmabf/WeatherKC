c320ba57a9e6f930692a0337233675339df17651
Irma <irblancofdz@gmail.com>
2018-05-30 01:57:42 +0200

    Use by lazy to set up forecast image

--- app/src/main/java/com/example/irmablanco/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/irmablanco/weatherapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.irmablanco.weatherapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.provider.ContactsContract
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -10,25 +11,23 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity(){
     val TAG = MainActivity::class.java.canonicalName
-    var forecastImage: ImageView? = null
+    val forecastImage by lazy {
+        findViewById<ImageView>(R.id.forecast_image)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //C
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val europeanButton = findViewById<Button>(R.id.european_system_button)
         val americanButton = findViewById<Button>(R.id.american_system_button)
 
-        forecastImage = findViewById(R.id.forecast_image)
-        
         europeanButton?.setOnClickListener {
-            forecastImage?.setImageResource(R.drawable.offline_weather)
+            forecastImage.setImageResource(R.drawable.offline_weather)
         }
         americanButton?.setOnClickListener{
-            forecastImage?.setImageResource(R.drawable.offline_weather2)
+            forecastImage.setImageResource(R.drawable.offline_weather2)
         }
-
     }
 
     override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
