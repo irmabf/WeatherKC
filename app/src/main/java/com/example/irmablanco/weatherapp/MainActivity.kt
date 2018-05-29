dad5e63a898c9a1d5c2b92c943c0e23c14c0f08b
Irma <irblancofdz@gmail.com>
2018-05-30 01:47:12 +0200

    Change american system image on onclick event

--- app/src/main/java/com/example/irmablanco/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/irmablanco/weatherapp/MainActivity.kt
@@ -6,9 +6,11 @@ import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     val TAG = MainActivity::class.java.canonicalName
+    var forecastImage: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         //C
         super.onCreate(savedInstanceState)
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val europeanButton = findViewById<Button>(R.id.european_system_button)
         val americanButton = findViewById<Button>(R.id.american_system_button)
+        forecastImage = findViewById(R.id.forecast_image)
 
         europeanButton?.setOnClickListener(this)
         americanButton?.setOnClickListener(this)
@@ -29,10 +32,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        Log.v(TAG, when (v?.id){
+        /*Log.v(TAG, when (v?.id){
             R.id.european_system_button ->  "Han pulsado el boton europeo"
             R.id.american_system_button -> "Han pulsado el boton americano"
             else -> "No se lo que han pulsado"
-        })
+        })*/
+        val imageToShow = when (v?.id){
+            R.id.european_system_button -> R.drawable.offline_weather
+            else -> R.drawable.offline_weather2
+        }
+        forecastImage?.setImageResource(imageToShow)
     }
 }
