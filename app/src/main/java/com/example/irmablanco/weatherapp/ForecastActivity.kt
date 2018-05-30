2e328e28c562df5691ca5b47e1e9d5100abd4075
Irma <irblancofdz@gmail.com>
2018-05-30 20:40:47 +0200

    Add snackbar

--- app/src/main/java/com/example/irmablanco/weatherapp/ForecastActivity.kt
+++ app/src/main/java/com/example/irmablanco/weatherapp/ForecastActivity.kt
@@ -6,8 +6,11 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.support.design.widget.Snackbar
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_forecast.*
 
 class ForecastActivity : AppCompatActivity() {
@@ -88,6 +91,11 @@ class ForecastActivity : AppCompatActivity() {
 
                     // Actualizo la interfaz con las nuevas unidades
                     updateTemperatureView()
+
+                    val newUnitsString = if (newUnits == TemperatureUnit.CELSIUS) getString(R.string.user_selects_celsius)
+                        else getString(R.string.user_selects_fahrenheit)
+                    //Toast.makeText(this, newUnitsString, Toast.LENGTH_LONG).show()
+                    Snackbar.make(findViewById<View>(android.R.id.content), newUnitsString, Snackbar.LENGTH_LONG).show()
                 }
             }
         }
