d93445dbfbb77ed1ea01fdf409b535fab58ec97b
Irma <irblancofdz@gmail.com>
2018-05-30 11:30:40 +0200

    Add forecast setter

--- app/src/main/java/com/example/irmablanco/weatherapp/ForecastActivity.kt
+++ app/src/main/java/com/example/irmablanco/weatherapp/ForecastActivity.kt
@@ -7,26 +7,34 @@ import kotlinx.android.synthetic.main.activity_forecast.*
 import kotlin.math.min
 
 class ForecastActivity : AppCompatActivity(){
-    val TAG = ForecastActivity::class.java.canonicalName
+
+    companion object {
+        val TAG = ForecastActivity::class.java.canonicalName
+    }
+
+    var forecast: Forecast? = null
+        //Este codigo, el seter, se ejecutara cuandos se actualize la interfaz ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓📌
+        set(value) {
+            if (value != null){
+                forecast_image.setImageResource(value.icon)
+                forecast_description.text = value.description
+                max_temp.text = getString(R.string.max_temp_format, value.maxTemp)
+                min_temp.text = getString(R.string.min_temp_format, value.minTemp)
+                humidity.text = getString(R.string.humidity_format, value.humidity)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
-
-        val forecast = Forecast(
+        //📌↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑Este codigo me setea el var forecast
+        forecast = Forecast(
                 25f,
                 10f,
                 35f,
                 "Soleado con alguna nube",
                 R.drawable.ico_01)
 
-        setForecast(forecast)
     }
-    fun setForecast(forecast: Forecast){
-        forecast_image.setImageResource(forecast.icon)
-        forecast_description.text = forecast.description
-        max_temp.text = getString(R.string.max_temp_format, forecast.maxTemp)
-        min_temp.text = getString(R.string.min_temp_format, forecast.minTemp)
-        humidity.text = getString(R.string.humidity_format, forecast.humidity)
-    }
+
 }
