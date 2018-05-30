542a4bb2a9e1ca1783f600d2782cdda31bee9e2b
Irma <irblancofdz@gmail.com>
2018-05-30 21:26:13 +0200

    Pass unit test for maxTemp and minTemp

---
+++ app/src/test/java/com/example/irmablanco/weatherapp/ForecastUnitTest.kt
@@ -0,0 +1,28 @@
+package com.example.irmablanco.weatherapp
+
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.Before
+
+class ForecastUnitTest {
+    lateinit var  forecast: Forecast
+
+    @Before
+    fun setUp(){
+        forecast = Forecast(
+                25f,
+                10f,
+                35f,
+                "Soleado con alguna nube",
+                R.drawable.ico_01)
+    }
+    @Test
+    fun maxTempUnitsConversion_isCorrect(){
+        assertEquals(77f, forecast.getMaxTemp(TemperatureUnit.FAHRENHEIT))
+
+    }
+    @Test
+    fun minTempUnitsConversion_isCorrect(){
+        assertEquals(50f, forecast.getMinTemp(TemperatureUnit.FAHRENHEIT))
+    }
+}
