52fdd16c70dc2a5d60a38b4f89c67909df8d51e3
Irma <irblancofdz@gmail.com>
2018-06-01 02:35:52 +0200

    Add pager

--- app/src/main/java/com/example/irmablanco/weatherapp/activity/CityPagerActivity.kt
+++ app/src/main/java/com/example/irmablanco/weatherapp/activity/CityPagerActivity.kt
@@ -1,12 +1,33 @@
 package com.example.irmablanco.weatherapp.activity
 
+import android.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v13.app.FragmentPagerAdapter
+import com.example.irmablanco.weatherapp.R
+import com.example.irmablanco.weatherapp.fragment.ForecastFragment
+import com.example.irmablanco.weatherapp.model.Cities
+import kotlinx.android.synthetic.main.activity_city_pager.*
 
 class CityPagerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city_pager)
+        val cities = Cities()
+        //Creo el adaptador
+        val adapter = object: FragmentPagerAdapter(fragmentManager){
+
+            override fun getItem(position: Int): Fragment {
+                return ForecastFragment()
+            }
+            /*override fun getCount(): Int {
+                return cities.count
+            }*/
+            override fun getCount() = cities.count
+        }
+        //Paso el adaptador al fragment
+        //Ojo!! Vamos a android manifest a informar de que esta es la priemra pantalla
+        view_pager.adapter = adapter
     }
 }
