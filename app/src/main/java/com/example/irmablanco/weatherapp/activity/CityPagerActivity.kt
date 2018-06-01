48155122fc8e679ae452f9d0869db73bcd679cf0
Irma <irblancofdz@gmail.com>
2018-06-01 17:11:57 +0200

    Go to CityPagerActivity and tell which is the toolbar that we want by default. Set up a logo for the toolbar.

--- app/src/main/java/com/example/irmablanco/weatherapp/activity/CityPagerActivity.kt
+++ app/src/main/java/com/example/irmablanco/weatherapp/activity/CityPagerActivity.kt
@@ -15,6 +15,17 @@ class CityPagerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city_pager)
+
+        /**Decimos a la action bar cual es la toolbar que queremos que se use en nuestra app
+         * Qué vista es la que queremos que haga de toolbar, lo debemos decir con findViewById
+         * o con una kotlin extension como hacemos en este caso.
+         * ****Recordamos que en activity_city_pager hemos dado al widget toolbar un id de toolbar y esto es lo
+         * que pasamos
+         *
+         * Tambien vamos a cambiar el logo a la toolbar*/
+        toolbar.setLogo(R.mipmap.ic_launcher)
+        setSupportActionBar(toolbar)
+
         val cities = Cities()
         //Creo el adaptador
         val adapter = object: FragmentPagerAdapter(supportFragmentManager){
